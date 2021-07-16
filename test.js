c2043a066845758fc8b717a1c1ced404557f1c24
Jennifer Moesa <82216812+JenniferMoesa@users.noreply.github.com>
2021-07-16 20:45:06 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,33 @@
+// deel 1
+
+const age = 30;
+
+if (age >= 18) {
+    document.write("Je mag naar binnen in de kroeg.");
+
+} else {
+    document.write("Je bent te jong. Je mag helaas niet naar binnen.");
+}
+
+
+// deel 2
+
+const isFemale = "male";
+
+if (isFemale == "female") {
+    document.write(true);
+
+} else {
+    document.write(false);
+}
+
+
+// deel 3
+const driverStatus = "bob";
+
+if (driverStatus == "bob") {
+    document.write("Je mag rijden.");
+
+} else {
+    document.write("Je mag niet rijden.");
+}
